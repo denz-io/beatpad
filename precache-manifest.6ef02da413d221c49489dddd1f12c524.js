ec2ad9e3c574303a2c765132c8e9f911b1c5537e
aqui <aquiabuin@gmail.com>
2019-08-02 11:03:59 -0300

    Updates

---
+++ precache-manifest.6ef02da413d221c49489dddd1f12c524.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5785329548abb2a5872e3e99d7b577dc",
+    "url": "/drumpad/index.html"
+  },
+  {
+    "revision": "2295a464b84dd7b784ef",
+    "url": "/drumpad/static/css/main.0a8c2ad6.chunk.css"
+  },
+  {
+    "revision": "de733c464285703d005c",
+    "url": "/drumpad/static/js/2.2d8b9631.chunk.js"
+  },
+  {
+    "revision": "2295a464b84dd7b784ef",
+    "url": "/drumpad/static/js/main.3d82b6dd.chunk.js"
+  },
+  {
+    "revision": "25191e1c803b07a4ffd2",
+    "url": "/drumpad/static/js/runtime~main.ccb0b8bc.js"
+  },
+  {
+    "revision": "9ff277af8c2168685c52b911c406cf64",
+    "url": "/drumpad/static/media/Bass-Drop.9ff277af.flac"
+  },
+  {
+    "revision": "2f210dd997341e7c182c990e582c1183",
+    "url": "/drumpad/static/media/Bass-Loop-01.2f210dd9.flac"
+  },
+  {
+    "revision": "ea10a1956f90aaaa3dce73922e07d5e1",
+    "url": "/drumpad/static/media/Bass-Loop-02.ea10a195.flac"
+  },
+  {
+    "revision": "2745ee13b28c06994c3645de8d5101ba",
+    "url": "/drumpad/static/media/Bling-Bling-FX.2745ee13.flac"
+  },
+  {
+    "revision": "06901c6d0f20c4bc8e187076fca0b7d7",
+    "url": "/drumpad/static/media/Bling-Bling-Snare.06901c6d.flac"
+  },
+  {
+    "revision": "f93d9015fa079c4021da2003964fc171",
+    "url": "/drumpad/static/media/Butterfly-Clap-01.f93d9015.flac"
+  },
+  {
+    "revision": "792b4c0c23e169aaa26edd6424b2909c",
+    "url": "/drumpad/static/media/Butterfly-Hat-01.792b4c0c.flac"
+  },
+  {
+    "revision": "c38d6963731351d09f909cfb5450f1f2",
+    "url": "/drumpad/static/media/Butterfly-Kick-04.c38d6963.flac"
+  },
+  {
+    "revision": "b1347a4c2571afe1ed40dc7e0221e70b",
+    "url": "/drumpad/static/media/Butterfly-Snare-01.b1347a4c.flac"
+  },
+  {
+    "revision": "52d45bfa27ce2e72478567da85bed394",
+    "url": "/drumpad/static/media/Wings-Snap-01.52d45bfa.flac"
+  },
+  {
+    "revision": "a8c42a2a5b31d7c1a44afe674d330375",
+    "url": "/drumpad/static/media/Wings-Vox-01.a8c42a2a.flac"
+  },
+  {
+    "revision": "ec750b07b39016d35a11d5e93a96ab50",
+    "url": "/drumpad/static/media/Wings-Vox-02.ec750b07.flac"
+  }
+]);
