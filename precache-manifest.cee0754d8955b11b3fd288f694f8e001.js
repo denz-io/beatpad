4a40bff36ad38b3e0c4c091b57ea300f221cb64c
denzdacs <denzehldacuyan@gmail.com>
2019-08-16 21:13:30 +0800

    Updates

---
+++ precache-manifest.cee0754d8955b11b3fd288f694f8e001.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6105ace67e15906ab32afb42ee565ee",
+    "url": "/beatpad/index.html"
+  },
+  {
+    "revision": "30ff302eaf721d96a06f",
+    "url": "/beatpad/static/css/main.1753f8fe.chunk.css"
+  },
+  {
+    "revision": "327d75eb36747cecbd8d",
+    "url": "/beatpad/static/js/2.83172970.chunk.js"
+  },
+  {
+    "revision": "30ff302eaf721d96a06f",
+    "url": "/beatpad/static/js/main.53205068.chunk.js"
+  },
+  {
+    "revision": "15cad7c95cfea9a3e2d5",
+    "url": "/beatpad/static/js/runtime~main.68ca6a1d.js"
+  },
+  {
+    "revision": "9ff277af8c2168685c52b911c406cf64",
+    "url": "/beatpad/static/media/Bass-Drop.9ff277af.flac"
+  },
+  {
+    "revision": "2f210dd997341e7c182c990e582c1183",
+    "url": "/beatpad/static/media/Bass-Loop-01.2f210dd9.flac"
+  },
+  {
+    "revision": "ea10a1956f90aaaa3dce73922e07d5e1",
+    "url": "/beatpad/static/media/Bass-Loop-02.ea10a195.flac"
+  },
+  {
+    "revision": "2745ee13b28c06994c3645de8d5101ba",
+    "url": "/beatpad/static/media/Bling-Bling-FX.2745ee13.flac"
+  },
+  {
+    "revision": "06901c6d0f20c4bc8e187076fca0b7d7",
+    "url": "/beatpad/static/media/Bling-Bling-Snare.06901c6d.flac"
+  },
+  {
+    "revision": "f93d9015fa079c4021da2003964fc171",
+    "url": "/beatpad/static/media/Butterfly-Clap-01.f93d9015.flac"
+  },
+  {
+    "revision": "792b4c0c23e169aaa26edd6424b2909c",
+    "url": "/beatpad/static/media/Butterfly-Hat-01.792b4c0c.flac"
+  },
+  {
+    "revision": "c38d6963731351d09f909cfb5450f1f2",
+    "url": "/beatpad/static/media/Butterfly-Kick-04.c38d6963.flac"
+  },
+  {
+    "revision": "b1347a4c2571afe1ed40dc7e0221e70b",
+    "url": "/beatpad/static/media/Butterfly-Snare-01.b1347a4c.flac"
+  },
+  {
+    "revision": "52d45bfa27ce2e72478567da85bed394",
+    "url": "/beatpad/static/media/Wings-Snap-01.52d45bfa.flac"
+  },
+  {
+    "revision": "a8c42a2a5b31d7c1a44afe674d330375",
+    "url": "/beatpad/static/media/Wings-Vox-01.a8c42a2a.flac"
+  },
+  {
+    "revision": "ec750b07b39016d35a11d5e93a96ab50",
+    "url": "/beatpad/static/media/Wings-Vox-02.ec750b07.flac"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/drumpad/precache-manifest.e25f57c69543d19d73612451f26446fd.js"
+  "/beatpad/precache-manifest.cee0754d8955b11b3fd288f694f8e001.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/drumpad/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/beatpad/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
